3e7dcdf50307a9c8973985fd805aff61213608fd
cosmonaut <evan@moonside.games>
2024-06-14 14:57:56 -0700

    fix UBO define

--- src/metal/Refresh_metal.m
+++ src/metal/Refresh_metal.m
@@ -35,7 +35,7 @@
 
 #define METAL_MAX_BUFFER_COUNT 31
 #define WINDOW_PROPERTY_DATA "Refresh_MetalWindowPropertyData"
-#define UBO_BUFFER_SIZE 1048576 /* 1 MiB */
+#define UNIFORM_BUFFER_SIZE 1048576 /* 1 MiB */
 #define REFRESH_SHADERSTAGE_COMPUTE 2
 
 #define EXPAND_ARRAY_IF_NEEDED(arr, elementType, newCount, capacity, newCapacity)    \
